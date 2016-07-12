e1481e8659f16d33277b8365f67063b4183989a1
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-07-12 10:53:43 +0100

    first test passed, added second test

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 feature 'restaurants' do
 	context 'no restaurants have been added' do
 		scenario 'should display a prompt to add a restaurant' do
-			visit '/restaurants'
-			expect(page).to have_link('Add restaurant')
+			visit '/restaurants/index'
+			expect(page).to have_link('add_restaurant')
 		end
+
+    scenario 'should take to a page to add a new restaurant' do
+      visit '/restaurants/index'
+      click_link('add_restaurant')
+      expect(page).to have_content('Create a Restaurant')
+    end
 	end
 end
