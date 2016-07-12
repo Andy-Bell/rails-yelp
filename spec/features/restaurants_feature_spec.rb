a8d40bcf935ca4521dfece42a039847448c1efdb
mtaner <mervetaner88@gmail.com>
2016-07-12 11:22:59 +0100

    Adds test for displaying restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,14 +3,28 @@ require 'rails_helper'
 feature 'restaurants' do
 	context 'no restaurants have been added' do
 		scenario 'should display a prompt to add a restaurant' do
-			visit '/restaurants/index'
+			visit '/restaurants'
 			expect(page).to have_link('add_restaurant')
 		end
 
     scenario 'should take to a page to add a new restaurant' do
-      visit '/restaurants/index'
+      visit '/restaurants'
       click_link('add_restaurant')
       expect(page).to have_content('Create a Restaurant')
     end
 	end
+
+	context 'restaurants have been added' do
+			before do
+				Restaurant.create(name: 'Lupita')
+			end
+
+			scenario 'display restaurants' do
+				visit '/restaurants'
+				expect(page).to have_content('Lupita')
+			end
+
+	end
+
+
 end
