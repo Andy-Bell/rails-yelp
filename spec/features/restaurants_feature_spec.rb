03986e827533d609efccb25de03730301359dc26
Andy <andy.bell.github@gmail.com>
2016-07-12 12:08:02 +0100

    passed test 3, added test 4

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,30 +1,42 @@
 require 'rails_helper'
 
 feature 'restaurants' do
-	context 'no restaurants have been added' do
-		scenario 'should display a prompt to add a restaurant' do
-			visit '/restaurants'
-			expect(page).to have_link('add_restaurant')
-		end
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_link('add_restaurant')
+    end
 
     scenario 'should take to a page to add a new restaurant' do
       visit '/restaurants'
       click_link('add_restaurant')
       expect(page).to have_content('Create a Restaurant')
     end
-	end
+  end
 
-	context 'restaurants have been added' do
-			before do
-				Restaurant.create(name: 'Lupita')
-			end
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Lupita')
+    end
 
-			scenario 'display restaurants' do
-				visit '/restaurants'
-				expect(page).to have_content('Lupita')
-			end
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Lupita')
+    end
 
-	end
+  end
+
+  context 'restaurant creation' do
+    scenario 'allows creation of restaurant' do
+      visit '/restaurants'
+      click_link('add_restaurant')
+      fill_in('restaurant_name', with: 'Pizza Express')
+      fill_in('restaurant_description', with: 'Makes pizza and pasta')
+      click('submit')
+      expect(page).to have_content('Pizza Express')
+      expect(page).to have_content('Makes pizza and pasta')
+    end
+  end
 
 
 end
