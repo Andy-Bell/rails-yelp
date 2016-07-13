51310723768be5b721a255a7c26649d8874d342f
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-07-13 13:57:01 +0100

    Welcome message with user email when signed id

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -20,6 +20,11 @@ feature 'User sessions' do
       sign_up
     end
 
+    scenario 'user\'s email address is displayed' do
+      visit '/'
+      expect(page).to have_content 'Welcome test@test.com'
+    end
+
     scenario 'sign in and sign up links are not displayed' do
       visit '/'
       expect(page).not_to have_link 'Sign up'
