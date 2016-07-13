85f13960a1f0c109fa161c7b0331f096efb57d05
srp2930 <s.rimingtonpounder@gmail.com>
2016-07-13 10:47:05 +0100

    add unit test for restaurant validation

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,1 +1,11 @@
-require 'spec_helper'
+require 'rails_helper'
+
+describe 'Restaurant' do
+  describe 'validation' do
+    it 'is not valid with a name of less than three letters' do
+      restaurant = Restaurant.new(name: 'KF')
+      expect(restaurant).to have(1).error_on(:name)
+      expect(restaurant).not_to be_valid
+    end
+  end
+end
