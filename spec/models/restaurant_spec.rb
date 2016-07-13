ca79f3e104a051c341a6f302de7e311f751479e4
srp2930 <s.rimingtonpounder@gmail.com>
2016-07-13 11:05:51 +0100

    Adds validation for review rating - cannot be greater than 5 - and validation for uniqueness of restaurant name

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -7,5 +7,12 @@ describe 'Restaurant' do
       expect(restaurant).to have(1).error_on(:name)
       expect(restaurant).not_to be_valid
     end
+
+    it 'is not valid unless it has a unique name' do
+      Restaurant.create(name: 'Torinos')
+      restaurant = Restaurant.new(name: 'Torinos')
+      expect(restaurant).to have(1).error_on(:name)
+      expect(restaurant).not_to be_valid
+    end
   end
 end
